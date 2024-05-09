09d4924301e5144d58ea816ddf679db251cf1b89
Gabriela <gadisa10@outlook.com>
2024-05-09 12:39:45 -0300

    🔥 chore: remoção do template inicial do angular e direcionamento para a page home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  // Rota para capturar caminhos não correspondentes
+  {
+    path: '**',
+    redirectTo: '/home',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
