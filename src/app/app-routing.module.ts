ae83fe2f6f313b5f32b3d7ad81cd0eeea7557288
José Fernando Leal da Cruz <Fernando.Leal@jala.university>
2024-05-17 19:16:36 -0300

    adicionando tela de cadastro - já pode ser acessado pela url com /cadastro - adicionado imagem ícone de usuário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
   },
+  {
+    path: 'cadastro',
+    component: TelaCadastroComponent,
+  },
   // Rota para capturar caminhos não correspondentes
   {
     path: '**',
     redirectTo: '/home',
-  },
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    TelaCadastroComponent
   ],
   imports: [
     BrowserModule,
