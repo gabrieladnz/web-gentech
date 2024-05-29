b55b20ccc3ef4484dc643808941276f573e30b3e
Gabriela <gadisa10@outlook.com>
2024-05-29 17:26:15 -0300

    💄 style: criação dos botões de categoria

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  // objeto temporário
+  protected listaCategorias = [
+    {
+      categoria: 'IA'
+    },
+    {
+      categoria: 'Computação'
+    },
+    {
+      categoria: 'Machine Learning'
+    }
+  ];
 
+  public constructor() { }
 }
