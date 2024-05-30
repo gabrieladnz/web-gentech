ae57a5fdbbaec16455f7d31b90c5f0fba905f011
Gabriela Diniz <gadisa10@outlook.com>
2024-05-30 19:52:48 -0300

    📱 style: responsividade da tela de artigos para tablet e mobile

--- src/app/pages/artigos/artigos.component.ts
+++ src/app/pages/artigos/artigos.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-artigos',
   templateUrl: './artigos.component.html',
-  styleUrl: './artigos.component.scss'
+  styleUrl: './artigos.component.scss',
 })
 export class ArtigosComponent {
+  constructor(private location: Location) {}
 
+  protected retornarPagina(): void {
+    this.location.back();
+  }
 }
