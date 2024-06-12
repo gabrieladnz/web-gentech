d65701c9c2b64f3fcd9ee8cf5e84ce934c240f85
Gabriela <gadisa10@outlook.com>
2024-06-12 13:02:27 -0300

    ✨feat: integração da funcionalidade de criar fóruns

--- src/app/components/modais/modal-publicar-forum/modal-publicar-forum.component.ts
+++ src/app/components/modais/modal-publicar-forum/modal-publicar-forum.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { UserServiceService } from '../../../services/user/user-service.service';
+import { ErrorResponse } from '../../../models/http/interface-http';
 
 @Component({
   selector: 'app-modal-publicar-forum',
@@ -9,13 +10,31 @@ import { UserServiceService } from '../../../services/user/user-service.service'
   styleUrl: './modal-publicar-forum.component.scss',
 })
 export class ModalPublicarForumComponent {
+  protected criarForumForm!: FormGroup;
+  public errorMessage = '';
+
   public constructor(
     public dialogRef: MatDialogRef<ModalPublicarForumComponent>,
     private userService: UserServiceService,
     private formBuilder: FormBuilder
-  ) {}
+  ) {
+    this.criarForumForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+    })
+  }
 
   protected cancelarCriacao(): void {
     this.dialogRef.close();
+  }
+
+  protected async criarForum(): Promise<void> {
+    try {
+      await this.userService.criarForum(this.criarForumForm.value);
+    } catch (error) {
+      this.errorMessage = `${(error as ErrorResponse).message}`;
+    } finally {
+      this.cancelarCriacao();
+    }
   }
 }
